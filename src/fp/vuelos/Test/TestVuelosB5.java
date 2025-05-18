aee89c622190c3784ae654b8689583323331f92b
Cune36 <curroch3006@gmail.com>
2025-05-18 12:56:55 +0200

    Bloque 5 completo excepto funciones irrrelevantes

--- src/fp/vuelos/Test/TestVuelosB5.java
+++ src/fp/vuelos/Test/TestVuelosB5.java
@@ -2,6 +2,8 @@ package fp.vuelos.Test;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.SortedSet;
 
 import fp.vuelos.FactoriaVuelos;
@@ -30,13 +32,110 @@ public class TestVuelosB5 {
 		}
 	}
 	
+	private static void TestGetDestinosOrdenados(VuelosB5 vuelos, Character c) {
+		SortedSet<String> res = vuelos.getDestinosOrdenados(c);
+		System.out.println("Los destinos ordenados son: ");
+		for(String a:res) {
+			System.out.println(a);
+		}
+	}
+	
+	private static void TestGetDestinosOrdenadosPorLongitud(VuelosB5 vuelos){
+		SortedSet<String> res = vuelos.getDestinosOrdenadosPorLongitud();
+		System.out.println("Los destinos ordenados son: ");
+		for(String a:res) {
+			System.out.println(a);
+		}
+	}
+	
+	private static void TestGetNumVuelosFecha(VuelosB5 vuelos, LocalDate fecha) {
+		Integer res = vuelos.getNumVuelosFecha(fecha);
+		System.out.println("El numero de vuelos en la fecha " + fecha + " es: " + res);
+	}
+	
+	private static void TestGetPrecioMedioVuelosMes(VuelosB5 vuelos, Integer mes) {
+		Double res = vuelos.getPrecioMedioVuelosMes(mes);
+		System.out.println("El precio medio en el mes " + mes + " es: " + res);
+	}
+	
+	private static void TestGetRecaudacion(VuelosB5 vuelos, Integer anyo) {
+		Double res = vuelos.getRecaudacion(anyo);
+		System.out.println("La recaudacion en el año " + anyo + " fue de " + res);
+	}
+	
+	private static void TestGetVueloMasPasajeros(VuelosB5 vuelos){
+		Vuelo res = vuelos.getVueloMasPasajeros();
+		System.out.println("El vuelo con mas pasajeros fue " + res);
+	}
+	
+	private static void TestGetVuelosPorFecha(VuelosB5 vuelos) {
+		Map<LocalDate, List<Vuelo>> res = vuelos.getVuelosPorFecha();
+		System.out.println(" Los vuelos por fecha son ");
+		for(LocalDate a:res.keySet()) {
+			System.out.println(a + "=" + res.get(a));
+		}
+	}
+	
+	private static void TestGetNumVuelosPorFecha(VuelosB5 vuelos) {
+		Map<LocalDate, Long> res = vuelos.getNumVuelosPorFecha();
+		System.out.println("El numero de vuelos por fecha es ");
+		for(LocalDate a:res.keySet()) {
+			System.out.println(a + "=" + res.get(a));
+		}
+	}
+	
+	private static void TestGetNumPlazasPorDestino(VuelosB5 vuelos) {
+		Map<String, Integer> res = vuelos.getNumPlazasPorDestino();
+		System.out.println(" El número de plazas por destino es ");
+		for(String a:res.keySet()) {
+			System.out.println(a + "=" + res.get(a));
+		}
+	}
+	
+	private static void TestGetPrecioMedioPorDestino(VuelosB5 vuelos) {
+		Map<String, Double> res = vuelos.getPrecioMedioPorDestino();
+		System.out.println(" El precio medio por destino es ");
+		for(String a:res.keySet()) {
+			System.out.println(a + "=" + res.get(a));
+		}
+	} 
+	
+	private static void TestGetFechasPorDestino(VuelosB5 vuelos) {
+		Map<String, Set<LocalDate>> res = vuelos.getFechasPorDestino();
+		System.out.println(" Las fechas por destino son:  ");
+		for(String a:res.keySet()) {
+			System.out.println(a + "=" + res.get(a));
+		}
+	}
+	
 	public static void main(String args[]) {
 		List<Vuelo> lista = TestFactoriaVuelos("aeropuerto.csv");
 		VuelosB5 vuelos = new VuelosB5("xd", lista);
-		System.out.println("===testGetNumDestinosDiferentesFecha ==========");
+		System.out.println("\n===testGetNumDestinosDiferentesFecha ==========");
 		TestGetNumDestinosDiferentesFecha(vuelos, LocalDate.of(2023, 06, 04));
-		System.out.println("===testGetVuelosOrdenados ==========");
+		System.out.println("\n===testGetVuelosOrdenados ==========");
 		TestGetVuelosOrdenados(vuelos);
-		System.out.println("===testGetDestinosOrdenados ==========");
+		System.out.println("\n===testGetDestinosOrdenados ==========");
+		TestGetDestinosOrdenados(vuelos, 'B');
+		System.out.println("\n===testGetDestinosOrdenadosPorLongitud ==========");
+		TestGetDestinosOrdenadosPorLongitud(vuelos);
+		System.out.println("\n===testGetNumVuelosFecha ==========");
+		TestGetNumVuelosFecha(vuelos, LocalDate.of(2023, 06, 04));
+		System.out.println("\n===testGetPrecioMedioVuelosMes ==========");
+		TestGetPrecioMedioVuelosMes(vuelos, 6);
+		System.out.println("\n===testGetRecaudacion ==========");
+		TestGetRecaudacion(vuelos, 2023);
+		System.out.println("\n===testGetVueloMasPasajeros ==========");
+		TestGetVueloMasPasajeros(vuelos);
+		System.out.println("\n===testGetVuelosPorFecha ==========");
+		TestGetVuelosPorFecha(vuelos);
+		System.out.println("\n===testGetNumVuelosPorFecha ==========");
+		TestGetNumVuelosPorFecha(vuelos);
+		System.out.println("\n===testGetNumPlazasPorDestino ==========");
+		TestGetNumPlazasPorDestino(vuelos);
+		System.out.println("\n===testGetPrecioMedioPorDestino ==========");
+		TestGetPrecioMedioPorDestino(vuelos);
+		System.out.println("\n===testGetFechasPorDestino ==========");
+		TestGetFechasPorDestino(vuelos);
 	}
 }
--- src/fp/vuelos/VuelosB5.java
+++ src/fp/vuelos/VuelosB5.java
@@ -1,10 +1,15 @@
 package fp.vuelos;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class VuelosB5 extends Vuelos{
@@ -29,5 +34,81 @@ public class VuelosB5 extends Vuelos{
 				.map(x-> x.getDestino())
 				.filter(x-> x.startsWith(String.valueOf(c)))
 				.collect(Collectors.toCollection(()-> new TreeSet<String>(Comparator.naturalOrder())));
+	}
+	
+	public SortedSet<String> getDestinosOrdenadosPorLongitud(){
+		return this.getVuelos().stream()
+				.map(x-> x.getDestino())
+				.collect(Collectors.toCollection(() -> new TreeSet<String>(Comparator.comparing(String::length)
+																			.thenComparing(Comparator.naturalOrder()))));
+				
+	}
+	public Integer getNumVuelosFecha(LocalDate fecha) {
+		return this.getVuelos().stream()
+				.filter(x-> x.getFecha().equals(fecha))
+				.collect(Collectors.toList()).size();
+	}
+	public Double getPrecioMedioVuelosMes(Integer mes) {
+		return this.getVuelos().stream()
+				.filter(x-> x.getFecha().getMonthValue() == (mes))
+				.mapToDouble(x-> x.getPrecio())
+				.average()
+				.orElse(0.0);
+	}
+	
+	public Double getRecaudacion(Integer anyo) {
+		return this.getVuelos().stream()
+				.filter(x-> x.getFecha().getYear() == anyo)
+				.mapToDouble(x-> x.getPrecio()*x.getNumPasajeros())
+				.sum();
+	}
+	
+	public Vuelo getVueloMasPasajeros() {
+		return Collections.max(this.getVuelos().stream()
+				.collect(Collectors.toList()),
+				Comparator.comparing(Vuelo::getNumPasajeros));
+	}
+	public Map<LocalDate, List<Vuelo>> getVuelosPorFecha(){
+		return this.getVuelos().stream()
+				.collect(Collectors.groupingBy(Vuelo::getFecha, Collectors.toList()));
+	}
+	
+	public Map<LocalDate, Long> getNumVuelosPorFecha(){
+		return this.getVuelos().stream()
+				.collect(Collectors.groupingBy(Vuelo::getFecha,
+												Collectors.counting()));
+	}
+	
+	public Map<String, Integer> getNumPlazasPorDestino(){
+		 //Funcion auxiliar que convierta un List<Vuelo> en el numero de plazas totales
+		Function<List<Vuelo>, Integer> listaVuelosAPlazasTotales = x-> x.stream()
+				.mapToInt(y -> y.getNumPlazas())
+				.sum();
+		 //Convertir en un Map<String, List<Vuelo>> con los vuelos por destino
+		return this.getVuelos().stream()
+				.collect(Collectors.groupingBy(Vuelo::getDestino, 
+						Collectors.collectingAndThen(Collectors.toList(), 
+								listaVuelosAPlazasTotales)));
+	}
+	
+	public Map<String, Double> getPrecioMedioPorDestino(){
+		//Funcion auxiliar usada
+		Function<List<Vuelo>, Double> ListaAMedia = x -> x.stream()
+				.mapToDouble(y -> y.getPrecio())
+				.average()
+				.orElse(0.0);
+		/*devolver Map<String, Double> a partir de uno cuyos valores son List<Vuelo> a las que se le aplica una aux que convierte en la
+		*media de precios
+		*/
+		return this.getVuelos().stream()
+				.collect(Collectors.groupingBy(Vuelo::getDestino, Collectors.collectingAndThen(Collectors.toList(), ListaAMedia)));
+	}
+	public Map<String, Set<LocalDate>> getFechasPorDestino(){
+		//Funcion
+		Function<List<Vuelo>, Set<LocalDate>> ListaVuelosToSetFechas = x -> x.stream()
+				.map(Vuelo::getFecha)
+				.collect(Collectors.toSet());
+		return this.getVuelos().stream()
+				.collect(Collectors.groupingBy(Vuelo::getDestino, Collectors.collectingAndThen(Collectors.toList(), ListaVuelosToSetFechas)));
 	}
 }
